82b45abc963932af991afa23ad59b862846ca2a2
grakovne <grakovne@gmail.com>
2023-11-04 00:59:40 +0100

    add scores

--- src/main/kotlin/org/grakovne/snake/GptStrategy.kt
+++ src/main/kotlin/org/grakovne/snake/GptStrategy.kt
@@ -133,7 +133,7 @@ class GptStrategy {
         return when {
             food.x == head.first && food.y == head.second -> Int.MAX_VALUE
             shortestPath.isEmpty() -> 0
-            else -> 1 - shortestPath.size + (0.2 * compactness).toInt()
+            else -> field.getWidth() * field.getHeight() - shortestPath.size + (0.5 * compactness).toInt()
         }
     }
 }
